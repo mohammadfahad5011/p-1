0db38bbc6214cda4d7cda470f3aac0dd5ea3afc8
Mohammad Fahad <85410237+mohammadfahad5011@users.noreply.github.com>
2021-09-05 14:17:51 -0600

    added html css and js

---
+++ mian.js
@@ -0,0 +1,49 @@
+// ---------owl-carousel-----
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 0,
+  nav: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+
+// ---------Js counter-----
+
+const counters = document.querySelectorAll(".counter");
+const speed = 200; // The lower the slower
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // Lower inc to slow and higher to slow
+    const inc = target / speed;
+
+    // console.log(inc);
+    // console.log(count);
+
+    // Check if target is reached
+    if (count < target) {
+      // Add inc to count and output in counter
+      counter.innerText = count + inc;
+      // Call function every ms
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
